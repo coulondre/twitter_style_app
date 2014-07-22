f1a940eed6658b68190d0d326f50e28c2c209e30
coulondre <arnaud.coulondre@gmail.com>
2014-07-22 16:51:54 +0200

    add a bug fix console, a user ressource, factories tests

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Arnaud Coulondre"
+    email    "arnaud.coulondre@gmail.com"
+    password "proutprout"
+    password_confirmation "proutprout"
+  end
+end
