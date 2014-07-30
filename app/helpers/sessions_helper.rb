1948fbff7ed47c504a93c1c902ce4ee9245d3be7
Arnaud Coulondre <arnaud.coulondre@gmail.com>
2014-07-30 15:03:55 +0200

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,7 @@
 module SessionsHelper
 
 	def sign_in(user)
-		remember_token = User.new_remember_token
+		  remember_token = User.new_remember_token
     	cookies.permanent[:remember_token] = remember_token
     	user.update_attribute(:remember_token, User.digest(remember_token))
     	self.current_user = user
@@ -26,4 +26,17 @@ module SessionsHelper
     	cookies.delete(:remember_token)
     	self.current_user = nil
   	end
+
+    def current_user?(user)
+      user == current_user
+    end
+
+    def redirect_back_or(default)
+      redirect_to(session[:return_to] || default)
+      session.delete(:return_to)
+    end
+
+    def store_location
+      session[:return_to] = request.url if request.get?
+    end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name     "Arnaud Coulondre"
-    email    "arnaud.coulondre@gmail.com"
-    password "proutprout"
-    password_confirmation "proutprout"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
   end
 end
