536d9333603d1eed503ba7617e8f69ea4bd53ec4
coulondre <arnaud.coulondre@gmail.com>
2014-07-31 16:39:18 +0200

    a uniqueness validation to user name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -68,8 +68,18 @@ describe User do
   describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
+      user_with_same_email.name = "test"
       user_with_same_email.email = @user.email.upcase
       user_with_same_email.save
+    end
+    it { should_not be_valid }
+  end
+
+  describe "when name is already taken" do
+    before do
+      user_with_same_name = @user.dup
+      user_with_same_name.email = "test@test.org" 
+      user_with_same_name.save
     end
     it { should_not be_valid }
   end
